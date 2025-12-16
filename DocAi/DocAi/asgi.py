f27a2f610e4597bd804e81e807b64ab478a42165
Shivani Pimpalshende <159690966+ShivaniPimpalshende@users.noreply.github.com>
2025-12-16 15:54:34 +0530

    Update asgi.py

--- DocAi/DocAi/asgi.py
+++ DocAi/DocAi/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DocAi.DocAi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DocAi.settings')
 
 application = get_asgi_application()
