d815ee95755fc409fbe646ed27ebf18059e0762b
Shivani Pimpalshende <159690966+ShivaniPimpalshende@users.noreply.github.com>
2025-12-16 15:52:14 +0530

    Update wsgi.py

--- DocAi/DocAi/wsgi.py
+++ DocAi/DocAi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DocAi.DocAi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DocAi.settings')
 
 application = get_wsgi_application()
